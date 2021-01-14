5b32523280db2683f67399eef71880e6a36e113e
BatBapt <lerouxbaptistepro2@gmail.com>
2021-01-14 22:41:34 +0100

    Main function adapted too

--- main.py
+++ main.py
@@ -8,19 +8,31 @@ from database import Database
 db = Database()
 db.init_db()
 
+p1 = Person()
+p1.first_name = "Patrick"
+p1.last_name = "LEROUX"
+p1.date_birth = "04/05/1970"
+p1.phone_number = "0304050607"
+p1.email = "patrick@gmail.com"
+c1 = Account()
+c1.owner = p1
+c1.balance = 950.0
 """
-p1 = Person("Paul", "LEROUX", "17-01-2001", "0203040506", "paul@gmail.com")
-c1 = Account(p1, 75.0)
-db.insert_person(p1)
-db.insert_account(c1)
-
-p2 = Person("Baptiste", "LEROUX", "23-07-1999", "0102030405", "baptiste@gmail.com")
-c2 = Account(p2, 75.0)
-db.insert_person(p2)
-db.insert_account(c2)
-"""
+good = db.insert_person(p1)
+if good > -1:
+    db.insert_account(c1)
 
 persons = db.display_all_person()
 for person in persons:
+    print("*"*40)
     print(person)
-    print("\n")
+
+print("\n")
+"""
+accounts = db.display_all_account()
+for account in accounts:
+    print("*"*40)
+    print(account)
+
+bene = db.display_all_beneficiaire(account.account_id)
+print(bene)
